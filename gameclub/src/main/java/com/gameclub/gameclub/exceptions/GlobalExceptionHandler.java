cda3896185a416602e37d48bdef1d490efb16f80
srihari-g-acharya-ci109 <2023ci_sriharigacharya_b@nie.ac.in>
2025-09-18 15:26:45 +0530

    Update GlobalExceptionHandler.java

--- gameclub/src/main/java/com/gameclub/gameclub/exceptions/GlobalExceptionHandler.java
+++ gameclub/src/main/java/com/gameclub/gameclub/exceptions/GlobalExceptionHandler.java
@@ -9,9 +9,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+
+
 public class GlobalExceptionHandler {
 
     private ResponseEntity<Object> buildResponse(Exception ex, HttpStatus status, HttpServletRequest request) {
